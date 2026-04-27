85c3ee1d178989bb42a46d1efc81c28585905c93
MaximUrban <64153150+MaximUrban@users.noreply.github.com>
2026-04-27 12:47:50 +0300

    Update device-library.ts

--- lib/device-library.ts
+++ lib/device-library.ts
@@ -128,6 +128,7 @@ function buildItem(params: {
   countries: LibraryCountry[];
   titleByCountry: Partial<Record<LibraryCountry, string>>;
   categoryLabelByCountry: Partial<Record<LibraryCountry, string>>;
+  productUrl?: string;
   drawingAssets?: DrawingAsset[];
 }): DeviceLibrarySourceItem {
   return {
@@ -143,6 +144,7 @@ function buildItem(params: {
       article: params.article,
       line2: params.category.toUpperCase(),
     }),
+    productUrl: params.productUrl,
     titleByCountry: params.titleByCountry,
     categoryLabelByCountry: params.categoryLabelByCountry,
     countries: params.countries,
@@ -176,7 +178,23 @@ function buildItem(params: {
 
 const staticDevices: DeviceLibrarySourceItem[] = [
   buildItem({
-    id: "sch-acti9-mcb-1p-c16-fr",
+    id: "fr-acti9-a9f74110",
+    series: "Acti9",
+    category: "mcb",
+    article: "A9F74110",
+    modules: 1,
+    countries: ["FR", "DE"],
+    titleByCountry: {
+      FR: "Acti9 disjoncteur 1P C10",
+      DE: "Acti9 Leitungsschutzschalter 1P C10",
+    },
+    categoryLabelByCountry: {
+      FR: "Автоматический выключатель",
+      DE: "Автоматический выключатель",
+    },
+  }),
+  buildItem({
+    id: "fr-acti9-a9f74116",
     series: "Acti9",
     category: "mcb",
     article: "A9F74116",
@@ -192,7 +210,39 @@ const staticDevices: DeviceLibrarySourceItem[] = [
     },
   }),
   buildItem({
-    id: "sch-acti9-rcd-2p-40a-30ma",
+    id: "fr-acti9-a9f74220",
+    series: "Acti9",
+    category: "mcb",
+    article: "A9F74220",
+    modules: 2,
+    countries: ["FR", "DE"],
+    titleByCountry: {
+      FR: "Acti9 disjoncteur 2P C20",
+      DE: "Acti9 Leitungsschutzschalter 2P C20",
+    },
+    categoryLabelByCountry: {
+      FR: "Автоматический выключатель",
+      DE: "Автоматический выключатель",
+    },
+  }),
+  buildItem({
+    id: "fr-acti9-a9r11225",
+    series: "Acti9",
+    category: "rcd",
+    article: "A9R11225",
+    modules: 2,
+    countries: ["FR", "DE"],
+    titleByCountry: {
+      FR: "Acti9 interrupteur différentiel 2P 25A 30mA",
+      DE: "Acti9 FI-Schutzschalter 2P 25A 30mA",
+    },
+    categoryLabelByCountry: {
+      FR: "Устройство защитного отключения",
+      DE: "Устройство защитного отключения",
+    },
+  }),
+  buildItem({
+    id: "fr-acti9-a9r11240",
     series: "Acti9",
     category: "rcd",
     article: "A9R11240",
@@ -210,17 +260,336 @@ const staticDevices: DeviceLibrarySourceItem[] = [
     },
   }),
   buildItem({
-    id: "sch-homeline-mcb-1p-20a-us",
+    id: "fr-acti9-a9d11816",
+    series: "Acti9",
+    category: "rcbo",
+    article: "A9D11816",
+    modules: 2,
+    countries: ["FR", "DE"],
+    titleByCountry: {
+      FR: "Acti9 disjoncteur différentiel 1P+N C16 30mA",
+      DE: "Acti9 FI/LS-Schalter 1P+N C16 30mA",
+    },
+    categoryLabelByCountry: {
+      FR: "Дифференциальный автомат",
+      DE: "Дифференциальный автомат",
+    },
+  }),
+
+  buildItem({
+    id: "us-homeline-hom120",
     series: "Homeline",
     category: "mcb",
     article: "HOM120",
     modules: 1,
     countries: ["US"],
     titleByCountry: {
-      US: "Homeline miniature circuit breaker 1P 20A",
+      US: "Homeline circuit breaker 1P 20A",
     },
     categoryLabelByCountry: {
       US: "Circuit breaker",
+    },
+  }),
+  buildItem({
+    id: "us-homeline-hom130",
+    series: "Homeline",
+    category: "mcb",
+    article: "HOM130",
+    modules: 1,
+    countries: ["US"],
+    titleByCountry: {
+      US: "Homeline circuit breaker 1P 30A",
+    },
+    categoryLabelByCountry: {
+      US: "Circuit breaker",
+    },
+  }),
+  buildItem({
+    id: "us-homeline-hom220",
+    series: "Homeline",
+    category: "mcb",
+    article: "HOM220",
+    modules: 2,
+    countries: ["US"],
+    titleByCountry: {
+      US: "Homeline circuit breaker 2P 20A",
+    },
+    categoryLabelByCountry: {
+      US: "Circuit breaker",
+    },
+  }),
+  buildItem({
+    id: "us-qo-qo120",
+    series: "QO",
+    category: "mcb",
+    article: "QO120",
+    modules: 1,
+    countries: ["US"],
+    titleByCountry: {
+      US: "QO circuit breaker 1P 20A",
+    },
+    categoryLabelByCountry: {
+      US: "Circuit breaker",
+    },
+  }),
+  buildItem({
+    id: "us-qo-qo230",
+    series: "QO",
+    category: "mcb",
+    article: "QO230",
+    modules: 2,
+    countries: ["US"],
+    titleByCountry: {
+      US: "QO circuit breaker 2P 30A",
+    },
+    categoryLabelByCountry: {
+      US: "Circuit breaker",
+    },
+  }),
+
+  buildItem({
+    id: "tr-ez9f34110",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F34110",
+    modules: 1,
+    countries: ["TR"],
+    productUrl: "https://www.se.com/tr/tr/product-range/61949-easy9-otomatik-sigorta-elektrik-sigorta-mcb-%C3%A7%C3%B6z%C3%BCmleri/",
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 1P 10A C",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f34116",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F34116",
+    modules: 1,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 1P 16A C",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f34120",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F34120",
+    modules: 1,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 1P 20A C",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f34125",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F34125",
+    modules: 1,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 1P 25A C",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f34132",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F34132",
+    modules: 1,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 1P 32A C",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f34216",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F34216",
+    modules: 2,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 2P 16A C",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f34225",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F34225",
+    modules: 2,
+    countries: ["TR"],
+    productUrl: "https://www.se.com/tr/tr/product/EZ9F34225/otomatik-sigorta-easy9-45ka-ce%C4%9Frisi-2kutup-25a/",
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 2P 25A C 4.5kA",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f34240",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F34240",
+    modules: 2,
+    countries: ["TR"],
+    productUrl: "https://www.se.com/tr/tr/product/EZ9F34240/otomatik-sigorta-easy9-45ka-ce%C4%9Frisi-2kutup-40a/",
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 2P 40A C 4.5kA",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f34263",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F34263",
+    modules: 2,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 minyatür devre kesici 2P 63A C 4.5kA",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f51210",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F51210",
+    modules: 2,
+    countries: ["TR"],
+    productUrl: "https://www.se.com/tr/tr/product/EZ9F51210/otomatik-sigorta-easy9-2p-10-a-c-curve-10000-a/",
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 2P 10A C 10kA",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f51240",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F51240",
+    modules: 2,
+    countries: ["TR"],
+    productUrl: "https://www.se.com/tr/tr/product/EZ9F51240/otomatik-sigorta-easy9-2p-40-a-c-curve-10000-a/",
+    titleByCountry: {
+      TR: "Easy9 otomatik sigorta 2P 40A C 10kA",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9f43406",
+    series: "Easy9",
+    category: "mcb",
+    article: "EZ9F43406",
+    modules: 4,
+    countries: ["TR"],
+    productUrl: "https://www.se.com/tr/tr/product/EZ9F43406/easy9-mcb-4p-6a-c-3000a-400v-otomatik-sigorta/",
+    titleByCountry: {
+      TR: "Easy9 MCB 4P 6A C 3000A 400V otomatik sigorta",
+    },
+    categoryLabelByCountry: {
+      TR: "Otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9r34240",
+    series: "Easy9",
+    category: "rcd",
+    article: "EZ9R34240",
+    modules: 2,
+    countries: ["TR"],
+    productUrl: "https://www.se.com/tr/tr/product/EZ9R34240/easy9-ka%C3%A7ak-ak%C4%B1m-r%C3%B6lesi-2-kutup-40a-30ma-ac-tipi-230v/",
+    titleByCountry: {
+      TR: "Easy9 kaçak akım rölesi 2P 40A 30mA AC",
+    },
+    categoryLabelByCountry: {
+      TR: "Kaçak akım koruma şalteri",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9r34263",
+    series: "Easy9",
+    category: "rcd",
+    article: "EZ9R34263",
+    modules: 2,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 kaçak akım koruma şalteri 2P 63A 30mA",
+    },
+    categoryLabelByCountry: {
+      TR: "Kaçak akım koruma şalteri",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9d34616",
+    series: "Easy9",
+    category: "rcbo",
+    article: "EZ9D34616",
+    modules: 2,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 diferansiyel otomatik sigorta 1P+N 16A 30mA",
+    },
+    categoryLabelByCountry: {
+      TR: "Diferansiyel otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9d34620",
+    series: "Easy9",
+    category: "rcbo",
+    article: "EZ9D34620",
+    modules: 2,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 diferansiyel otomatik sigorta 1P+N 20A 30mA",
+    },
+    categoryLabelByCountry: {
+      TR: "Diferansiyel otomatik sigorta",
+    },
+  }),
+  buildItem({
+    id: "tr-ez9d34625",
+    series: "Easy9",
+    category: "rcbo",
+    article: "EZ9D34625",
+    modules: 2,
+    countries: ["TR"],
+    titleByCountry: {
+      TR: "Easy9 diferansiyel otomatik sigorta 1P+N 25A 30mA",
+    },
+    categoryLabelByCountry: {
+      TR: "Diferansiyel otomatik sigorta",
     },
   }),
 ];
